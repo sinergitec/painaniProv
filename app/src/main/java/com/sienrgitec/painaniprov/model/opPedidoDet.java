bf99cbaeab37ecb6ac3a70a0f500e63f67c0cc12
cristian005 <cristian.aolc@gmail.com>
2020-04-28 22:58:27 -0500

    ok

---
+++ app/src/main/java/com/sienrgitec/painaniprov/model/opPedidoDet.java
@@ -0,0 +1,240 @@
+package com.sienrgitec.painaniprov.model;
+
+public class opPedidoDet {
+
+    private Integer iPedido;
+    private Integer iPedidoProv;
+    private Integer iPartida;
+    private String dtFecha;
+    private Integer iArticulo;
+    private String cArticulo;
+    private String cDescripcion;
+    private String cObs;
+    private Integer dePrecio;
+    private Integer dePorcImp;
+    private Integer deImpuesto;
+    private Integer dePrecioVta;
+    private Integer dePorcDescto;
+    private Integer deDescuento;
+    private Integer dePrecioUnit;
+    private Integer deCantidad;
+    private Integer deImporte;
+    private Integer deCantCancela;
+    private Object dtCancela;
+    private String dtCreado;
+    private Integer iRazon;
+    private String dtModificado;
+    private String cTipoRazon;
+    private String cUsuCrea;
+    private String cUsuModificado;
+    private Object id;
+
+
+    public Integer getiPedido() {
+        return iPedido;
+    }
+
+    public void setiPedido(Integer iPedido) {
+        this.iPedido = iPedido;
+    }
+
+    public Integer getiPedidoProv() {
+        return iPedidoProv;
+    }
+
+    public void setiPedidoProv(Integer iPedidoProv) {
+        this.iPedidoProv = iPedidoProv;
+    }
+
+    public Integer getiPartida() {
+        return iPartida;
+    }
+
+    public void setiPartida(Integer iPartida) {
+        this.iPartida = iPartida;
+    }
+
+    public String getDtFecha() {
+        return dtFecha;
+    }
+
+    public void setDtFecha(String dtFecha) {
+        this.dtFecha = dtFecha;
+    }
+
+    public Integer getiArticulo() {
+        return iArticulo;
+    }
+
+    public void setiArticulo(Integer iArticulo) {
+        this.iArticulo = iArticulo;
+    }
+
+    public String getcArticulo() {
+        return cArticulo;
+    }
+
+    public void setcArticulo(String cArticulo) {
+        this.cArticulo = cArticulo;
+    }
+
+    public String getcDescripcion() {
+        return cDescripcion;
+    }
+
+    public void setcDescripcion(String cDescripcion) {
+        this.cDescripcion = cDescripcion;
+    }
+
+    public String getcObs() {
+        return cObs;
+    }
+
+    public void setcObs(String cObs) {
+        this.cObs = cObs;
+    }
+
+    public Integer getDePrecio() {
+        return dePrecio;
+    }
+
+    public void setDePrecio(Integer dePrecio) {
+        this.dePrecio = dePrecio;
+    }
+
+    public Integer getDePorcImp() {
+        return dePorcImp;
+    }
+
+    public void setDePorcImp(Integer dePorcImp) {
+        this.dePorcImp = dePorcImp;
+    }
+
+    public Integer getDeImpuesto() {
+        return deImpuesto;
+    }
+
+    public void setDeImpuesto(Integer deImpuesto) {
+        this.deImpuesto = deImpuesto;
+    }
+
+    public Integer getDePrecioVta() {
+        return dePrecioVta;
+    }
+
+    public void setDePrecioVta(Integer dePrecioVta) {
+        this.dePrecioVta = dePrecioVta;
+    }
+
+    public Integer getDePorcDescto() {
+        return dePorcDescto;
+    }
+
+    public void setDePorcDescto(Integer dePorcDescto) {
+        this.dePorcDescto = dePorcDescto;
+    }
+
+    public Integer getDeDescuento() {
+        return deDescuento;
+    }
+
+    public void setDeDescuento(Integer deDescuento) {
+        this.deDescuento = deDescuento;
+    }
+
+    public Integer getDePrecioUnit() {
+        return dePrecioUnit;
+    }
+
+    public void setDePrecioUnit(Integer dePrecioUnit) {
+        this.dePrecioUnit = dePrecioUnit;
+    }
+
+    public Integer getDeCantidad() {
+        return deCantidad;
+    }
+
+    public void setDeCantidad(Integer deCantidad) {
+        this.deCantidad = deCantidad;
+    }
+
+    public Integer getDeImporte() {
+        return deImporte;
+    }
+
+    public void setDeImporte(Integer deImporte) {
+        this.deImporte = deImporte;
+    }
+
+    public Integer getDeCantCancela() {
+        return deCantCancela;
+    }
+
+    public void setDeCantCancela(Integer deCantCancela) {
+        this.deCantCancela = deCantCancela;
+    }
+
+    public Object getDtCancela() {
+        return dtCancela;
+    }
+
+    public void setDtCancela(Object dtCancela) {
+        this.dtCancela = dtCancela;
+    }
+
+    public String getDtCreado() {
+        return dtCreado;
+    }
+
+    public void setDtCreado(String dtCreado) {
+        this.dtCreado = dtCreado;
+    }
+
+    public Integer getiRazon() {
+        return iRazon;
+    }
+
+    public void setiRazon(Integer iRazon) {
+        this.iRazon = iRazon;
+    }
+
+    public String getDtModificado() {
+        return dtModificado;
+    }
+
+    public void setDtModificado(String dtModificado) {
+        this.dtModificado = dtModificado;
+    }
+
+    public String getcTipoRazon() {
+        return cTipoRazon;
+    }
+
+    public void setcTipoRazon(String cTipoRazon) {
+        this.cTipoRazon = cTipoRazon;
+    }
+
+    public String getcUsuCrea() {
+        return cUsuCrea;
+    }
+
+    public void setcUsuCrea(String cUsuCrea) {
+        this.cUsuCrea = cUsuCrea;
+    }
+
+    public String getcUsuModificado() {
+        return cUsuModificado;
+    }
+
+    public void setcUsuModificado(String cUsuModificado) {
+        this.cUsuModificado = cUsuModificado;
+    }
+
+    public Object getId() {
+        return id;
+    }
+
+    public void setId(Object id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/sienrgitec/painaniprov/model/opPedidoProveedor.java
@@ -0,0 +1,267 @@
+package com.sienrgitec.painaniprov.model;
+
+public class opPedidoProveedor {
+
+    private Integer iPedido;
+    private Integer iPedidoProv;
+    private Integer iProveedor;
+    private String dtFecha;
+    private Integer iHora;
+    private Integer iDomicilio;
+    private Integer deTotalPzas;
+    private Integer deSubTotal;
+    private Integer deImpuesto;
+    private Integer deImporte;
+    private String dtAvisado;
+    private Boolean lContestado;
+    private String dtContestado;
+    private Boolean lPagado;
+    private Object dtPagado;
+    private Boolean lVoBoPago;
+    private Double deAporta;
+    private Integer dePenalizacion;
+    private Double dePagoNeto;
+    private Boolean lCancelado;
+    private Object dtCancelado;
+    private Integer iRazon;
+    private Boolean lEntregado;
+    private String dtEntregado;
+    private String dtCreado;
+    private String dtModificado;
+    private String cUsuCrea;
+    private String cUsuModifica;
+    private Object id;
+
+
+    public Integer getiPedido() {
+        return iPedido;
+    }
+
+    public void setiPedido(Integer iPedido) {
+        this.iPedido = iPedido;
+    }
+
+    public Integer getiPedidoProv() {
+        return iPedidoProv;
+    }
+
+    public void setiPedidoProv(Integer iPedidoProv) {
+        this.iPedidoProv = iPedidoProv;
+    }
+
+    public Integer getiProveedor() {
+        return iProveedor;
+    }
+
+    public void setiProveedor(Integer iProveedor) {
+        this.iProveedor = iProveedor;
+    }
+
+    public String getDtFecha() {
+        return dtFecha;
+    }
+
+    public void setDtFecha(String dtFecha) {
+        this.dtFecha = dtFecha;
+    }
+
+    public Integer getiHora() {
+        return iHora;
+    }
+
+    public void setiHora(Integer iHora) {
+        this.iHora = iHora;
+    }
+
+    public Integer getiDomicilio() {
+        return iDomicilio;
+    }
+
+    public void setiDomicilio(Integer iDomicilio) {
+        this.iDomicilio = iDomicilio;
+    }
+
+    public Integer getDeTotalPzas() {
+        return deTotalPzas;
+    }
+
+    public void setDeTotalPzas(Integer deTotalPzas) {
+        this.deTotalPzas = deTotalPzas;
+    }
+
+    public Integer getDeSubTotal() {
+        return deSubTotal;
+    }
+
+    public void setDeSubTotal(Integer deSubTotal) {
+        this.deSubTotal = deSubTotal;
+    }
+
+    public Integer getDeImpuesto() {
+        return deImpuesto;
+    }
+
+    public void setDeImpuesto(Integer deImpuesto) {
+        this.deImpuesto = deImpuesto;
+    }
+
+    public Integer getDeImporte() {
+        return deImporte;
+    }
+
+    public void setDeImporte(Integer deImporte) {
+        this.deImporte = deImporte;
+    }
+
+    public String getDtAvisado() {
+        return dtAvisado;
+    }
+
+    public void setDtAvisado(String dtAvisado) {
+        this.dtAvisado = dtAvisado;
+    }
+
+    public Boolean getlContestado() {
+        return lContestado;
+    }
+
+    public void setlContestado(Boolean lContestado) {
+        this.lContestado = lContestado;
+    }
+
+    public String getDtContestado() {
+        return dtContestado;
+    }
+
+    public void setDtContestado(String dtContestado) {
+        this.dtContestado = dtContestado;
+    }
+
+    public Boolean getlPagado() {
+        return lPagado;
+    }
+
+    public void setlPagado(Boolean lPagado) {
+        this.lPagado = lPagado;
+    }
+
+    public Object getDtPagado() {
+        return dtPagado;
+    }
+
+    public void setDtPagado(Object dtPagado) {
+        this.dtPagado = dtPagado;
+    }
+
+    public Boolean getlVoBoPago() {
+        return lVoBoPago;
+    }
+
+    public void setlVoBoPago(Boolean lVoBoPago) {
+        this.lVoBoPago = lVoBoPago;
+    }
+
+    public Double getDeAporta() {
+        return deAporta;
+    }
+
+    public void setDeAporta(Double deAporta) {
+        this.deAporta = deAporta;
+    }
+
+    public Integer getDePenalizacion() {
+        return dePenalizacion;
+    }
+
+    public void setDePenalizacion(Integer dePenalizacion) {
+        this.dePenalizacion = dePenalizacion;
+    }
+
+    public Double getDePagoNeto() {
+        return dePagoNeto;
+    }
+
+    public void setDePagoNeto(Double dePagoNeto) {
+        this.dePagoNeto = dePagoNeto;
+    }
+
+    public Boolean getlCancelado() {
+        return lCancelado;
+    }
+
+    public void setlCancelado(Boolean lCancelado) {
+        this.lCancelado = lCancelado;
+    }
+
+    public Object getDtCancelado() {
+        return dtCancelado;
+    }
+
+    public void setDtCancelado(Object dtCancelado) {
+        this.dtCancelado = dtCancelado;
+    }
+
+    public Integer getiRazon() {
+        return iRazon;
+    }
+
+    public void setiRazon(Integer iRazon) {
+        this.iRazon = iRazon;
+    }
+
+    public Boolean getlEntregado() {
+        return lEntregado;
+    }
+
+    public void setlEntregado(Boolean lEntregado) {
+        this.lEntregado = lEntregado;
+    }
+
+    public String getDtEntregado() {
+        return dtEntregado;
+    }
+
+    public void setDtEntregado(String dtEntregado) {
+        this.dtEntregado = dtEntregado;
+    }
+
+    public String getDtCreado() {
+        return dtCreado;
+    }
+
+    public void setDtCreado(String dtCreado) {
+        this.dtCreado = dtCreado;
+    }
+
+    public String getDtModificado() {
+        return dtModificado;
+    }
+
+    public void setDtModificado(String dtModificado) {
+        this.dtModificado = dtModificado;
+    }
+
+    public String getcUsuCrea() {
+        return cUsuCrea;
+    }
+
+    public void setcUsuCrea(String cUsuCrea) {
+        this.cUsuCrea = cUsuCrea;
+    }
+
+    public String getcUsuModifica() {
+        return cUsuModifica;
+    }
+
+    public void setcUsuModifica(String cUsuModifica) {
+        this.cUsuModifica = cUsuModifica;
+    }
+
+    public Object getId() {
+        return id;
+    }
+
+    public void setId(Object id) {
+        this.id = id;
+    }
+}
